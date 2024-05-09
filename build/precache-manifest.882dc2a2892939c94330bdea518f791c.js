fe74a5e1bdc85652fb9477c7a438ac49c7f4811c
barbosageovanni <150721483+barbosageovanni@users.noreply.github.com>
2024-05-09 14:25:18 -0300

    Add files via upload

---
+++ build/precache-manifest.882dc2a2892939c94330bdea518f791c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70ccba8eb766cabce7f25b7238a1d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5d95bd0d9cabe819cf9",
+    "url": "/static/css/main.1bf7d063.chunk.css"
+  },
+  {
+    "revision": "6ceb3f660385b2696811",
+    "url": "/static/js/2.54a44b1b.chunk.js"
+  },
+  {
+    "revision": "ac2998ca55fd56aac3fe772329704f38",
+    "url": "/static/js/2.54a44b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d95bd0d9cabe819cf9",
+    "url": "/static/js/main.a59ee835.chunk.js"
+  },
+  {
+    "revision": "58c1606f69992c549f0a",
+    "url": "/static/js/runtime-main.9b514350.js"
+  }
+]);
